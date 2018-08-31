2645228eb16c271ee47c6dd4161d3c0e58d8205d
jaebooker <jaesonbooker@gmail.com>
2018-08-31 15:59:01 -0700

    deploy

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Good Morning, Starshine!</h1>
+    <p>The Earth says 'Hello, World!'</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
